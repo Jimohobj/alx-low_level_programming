3ca0f3980945a557df71041b3b42a668f83f1f60
Jimohobj <jimohobj1@gmail.com>
2023-06-19 18:02:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - A program that print new line, to the standard error.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	fput("place of art is usefull\" , Dora Korpar, 2015-10-2019\n", stdout);
+return (1);
+}
+
