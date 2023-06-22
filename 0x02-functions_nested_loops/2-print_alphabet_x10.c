45c7dfc2ed9d12d089ba048c3c3c40c2a3ab74d7
Jimohobj <jimohobj1@gmail.com>
2023-06-21 17:15:00 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,16 @@
 
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- *
  */
-
 void print_alphabet_x10(void)
 {
-int i;
-char ch;
-i = 0;
-while (i < 9)
-{
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-_putchar(ch);
-}
-_putchar('\n');
-i++;
-}
+	int count = 0;
+	char letter;
 
-
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
